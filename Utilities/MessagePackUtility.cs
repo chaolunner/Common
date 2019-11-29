492820260d00c40b193301f353a71a410c57362d
chaolun <chaolun.w@redrabbitanimation.cn>
2019-11-29 16:31:22 +0800

    - Fixed build target uses IL2CPP, you can not use Dynamic***Resolver so require to use pre-code generation problem

--- Utilities/MessagePackUtility.cs
+++ Utilities/MessagePackUtility.cs
@@ -4,40 +4,42 @@ namespace Common
 {
     public static class MessagePackUtility
     {
+        public static MessagePackSerializerOptions LZ4Standard = MessagePackSerializer.DefaultOptions.WithLZ4Compression(true);
+
         public static byte[] Serialize<T>(T obj, bool lz4 = true)
         {
             if (lz4)
             {
-                return MessagePackSerializer.Serialize(obj, MessagePackSerializerOptions.LZ4Standard);
+                return MessagePackSerializer.Serialize(obj, LZ4Standard);
             }
-            return MessagePackSerializer.Serialize(obj, MessagePackSerializerOptions.Standard);
+            return MessagePackSerializer.Serialize(obj);
         }
 
         public static T Deserialize<T>(byte[] bytes, bool lz4 = true)
         {
             if (lz4)
             {
-                return MessagePackSerializer.Deserialize<T>(bytes, MessagePackSerializerOptions.LZ4Standard);
+                return MessagePackSerializer.Deserialize<T>(bytes, LZ4Standard);
             }
-            return MessagePackSerializer.Deserialize<T>(bytes, MessagePackSerializerOptions.Standard);
+            return MessagePackSerializer.Deserialize<T>(bytes);
         }
 
         public static byte[] FromJson(string str, bool lz4 = true)
         {
             if (lz4)
             {
-                return MessagePackSerializer.ConvertFromJson(str, MessagePackSerializerOptions.LZ4Standard);
+                return MessagePackSerializer.ConvertFromJson(str, LZ4Standard);
             }
-            return MessagePackSerializer.ConvertFromJson(str, MessagePackSerializerOptions.Standard);
+            return MessagePackSerializer.ConvertFromJson(str);
         }
 
         public static string ToJson(byte[] bytes, bool lz4 = true)
         {
             if (lz4)
             {
-                return MessagePackSerializer.ConvertToJson(bytes, MessagePackSerializerOptions.LZ4Standard);
+                return MessagePackSerializer.ConvertToJson(bytes, LZ4Standard);
             }
-            return MessagePackSerializer.ConvertToJson(bytes, MessagePackSerializerOptions.Standard);
+            return MessagePackSerializer.ConvertToJson(bytes);
         }
     }
 }
