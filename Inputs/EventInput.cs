102e2aa924f2cd1f89e133f7f80d2563d4e75b3f
chaolun <chaolun.w@redrabbitanimation.cn>
2020-01-13 17:35:54 +0800

    - Update Event Input

--- Inputs/EventInput.cs
+++ Inputs/EventInput.cs
@@ -1,40 +1,19 @@
-﻿using System.Collections.Generic;
-using MessagePack;
+﻿using MessagePack;
 
 namespace Common
 {
     [MessagePackObject]
     public class EventInput : IInput
     {
-        private const char VerticalBar = '|';
-
         [Key(0)]
-        public Dictionary<EventCode, string> Events;
+        public EventCode Type;
+        [Key(1)]
+        public string Message;
 
-        public EventInput()
+        public EventInput(EventCode type, string msg)
         {
-            Events = new Dictionary<EventCode, string>();
-        }
-
-        public void Write(EventCode type, string msg)
-        {
-            if (!Events.ContainsKey(type))
-            {
-                Events.Add(type, msg);
-            }
-            else
-            {
-                Events[type] += VerticalBar + msg;
-            }
-        }
-
-        public string[] Read(EventCode type)
-        {
-            if (Events.ContainsKey(type))
-            {
-                return Events[type].Split(VerticalBar);
-            }
-            return null;
+            Type = type;
+            Message = msg;
         }
     }
 }
