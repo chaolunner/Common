d5d290da71e9cdb4483bf2fb689c47ad77a7a885
chaolun <0chaolun0@gmail.com>
2020-02-14 15:21:50 +0800

    - Update Receive function

---
+++ Core/ReceiveData.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+
+namespace Common
+{
+    public struct ReceiveData
+    {
+        public byte[] Value;
+        public SessionMode Mode;
+
+        public string StringValue
+        {
+            get
+            {
+                return Encoding.UTF8.GetString(Value);
+            }
+        }
+
+        public ReceiveData(byte[] value, SessionMode mode)
+        {
+            Value = value;
+            Mode = mode;
+        }
+    }
+}
