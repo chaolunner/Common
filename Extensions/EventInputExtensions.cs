28b1efb4bc444d207a0193127bf076ba85b8001a
chaolun <0chaolun0@gmail.com>
2020-02-28 11:17:01 +0800

    - Add EventInput Extensions

---
+++ Extensions/EventInputExtensions.cs
@@ -0,0 +1,45 @@
+﻿namespace Common
+{
+    public static class EventInputExtensions
+    {
+        public static EventInput GetEventInput(this EventInput[] eventInputs, EventCode type)
+        {
+            for (int i = 0; i < eventInputs.Length; i++)
+            {
+                if (eventInputs[i].Type == type)
+                {
+                    return eventInputs[i];
+                }
+            }
+            return null;
+        }
+
+        public static EventInput GetEventInput(this IInput[] inputs, EventCode type)
+        {
+            for (int i = 0; i < inputs.Length; i++)
+            {
+                var eventInput = inputs[i] as EventInput;
+                if (eventInput != null && eventInput.Type == type)
+                {
+                    return eventInput;
+                }
+            }
+            return null;
+        }
+
+        public static EventInput GetEventInput(this UserInputData[] userInputDatas, EventCode type, out int index)
+        {
+            index = -1;
+            for (int i = 0; i < userInputDatas.Length; i++)
+            {
+                var eventInput = GetEventInput(userInputDatas[i].Inputs, type);
+                if (eventInput != null)
+                {
+                    index = i;
+                    return eventInput;
+                }
+            }
+            return null;
+        }
+    }
+}
